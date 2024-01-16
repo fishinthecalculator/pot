e28a9cd25642a37fd9047f9b652600f2e9e00d87
Giacomo Leidi <giacomo.leidi@suse.com>
2024-01-16 22:10:30 +0100

    wip

---
+++ pot/oci/serialization/container.py
@@ -0,0 +1,73 @@
+from pot.oci.dataclasses.container import Container
+from pot.oci.serialization import DATETIME_FORMAT_STRING, ClassAdapter
+
+    @staticmethod
+    def from_dict(dict_object):
+        created = dict_object.get("CreatedAt", None)
+        if created:
+            created = Container.parse_created(created)
+        return Container(
+            container_id=dict_object["ID"] if "ID" in dict_object.keys() else dict_object["Id"],
+            command=dict_object["Command"],
+            image=dict_object["Image"],
+            created=created,
+            state=Container.parse_state(dict_object["State"]),
+            ports=Container.parse_ports(dict_object.get("Ports", [])),
+            mounts=Container.parse_mounts(dict_object.get("Mounts", [])),
+            name=dict_object.get("Names")
+        )
+
+    @staticmethod
+    def format_list_of_strings(list_of_strings: list[str]) -> str:
+        if not list_of_strings:
+            return None
+        return " ".join(list_of_strings)
+
+    @staticmethod
+    def _parse_list_of_strings(string: str | list[str]) -> list[str]:
+        if string:
+            if type(string) is list:
+                return string
+            else:
+                return string.split(" ")
+        else:
+            return []
+
+    @staticmethod
+    def parse_created(created: str) -> datetime:
+        return datetime.strptime(created, DATETIME_FORMAT_STRING)
+
+    @staticmethod
+    def parse_mounts(mounts: str) -> ContainerState:
+        return Container._parse_list_of_strings(mounts)
+
+    @staticmethod
+    def parse_ports(ports: str) -> ContainerState:
+        return Container._parse_list_of_strings(ports)
+
+    @staticmethod
+    def parse_state(state: str) -> ContainerState:
+        return ContainerState(state)
+
+    def to_tuple(self, spec) -> tuple:
+        return tuple(
+            getattr(self, a)
+            if a not in ["created", "mounts", "state", "ports"]
+            else getattr(self, f"format_{a}")()
+            for a in spec
+        )
+
+    def format_created(self) -> str:
+        if self.created:
+            return self.created.strftime(DATETIME_FORMAT_STRING)
+        else:
+            return ""
+
+    def format_ports(self):
+        return Container.format_list_of_strings(self.ports)
+
+    def format_mounts(self):
+        return Container.format_list_of_strings(self.mounts)
+
+    def format_state(self):
+        return self.state.value
